a75f47d00f1e83852a0656139c16d9ce15319467
eytor <eythoras16@ru.is>
2018-02-19 16:16:49 +0000

    a

--- part1/src/components/Login.js
+++ part1/src/components/Login.js
@@ -1,9 +1,41 @@
 import React from 'react';
 
 class Login extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      nickname:"",
+      error:""
+    };
+  }
+
+  handleSubmit = (e)=>{
+    e.preventDefault()
+
+
+  }
+
+  handleChange = ()=>{
+
+  }
     render() {
+      const { nickname, error} = this.state
         return (
-            <div className="container">{this.props.children}</div>
+            <div className="login">
+              <form onSubmit={this.handleSubmit} className="login-form">
+                <lable for="nickname"><h2>please type in nickname</h2></lable>
+                <input
+                  ref={(input)=>{this.textInput = input}}
+                  type="text"
+                  id="nickname"
+                  value={nickname}
+                  onchange={this.handleChange}
+                  placeholder={'Username'}
+                />
+              </form>
+            </div>
+            <div className="error">{error ? error:null}</div>
         )
     };
 };
